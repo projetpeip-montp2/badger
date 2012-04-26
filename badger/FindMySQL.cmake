b3c0c4b4c070d4ad2d7ce6de09b64c9ed2efb9fd
mecanix <victor.hiairrassary@gmail.com>
2012-04-26 17:59:42 +0200

    Ajout du support CMake sous Windows

--- badger/FindMySQL.cmake
+++ badger/FindMySQL.cmake
@@ -1,45 +1,46 @@
-# - Find mysqlclient
+# Find mysqlclient
 # Find the native MySQL includes and library
 #
-#  MYSQL_INCLUDE_DIR - where to find mysql.h, etc.
-#  MYSQL_LIBRARIES   - List of libraries when using MySQL.
-#  MYSQL_FOUND       - True if MySQL found.
+# MYSQL_INCLUDE_DIR - where to find mysql.h, etc.
+# MYSQL_LIBRARIES   - List of libraries when using MySQL.
+# MYSQL_FOUND       - True if MySQL found.
 
-IF (MYSQL_INCLUDE_DIR)
-    # Already in cache, be silent
-    SET(MYSQL_FIND_QUIETLY TRUE)
-ENDIF (MYSQL_INCLUDE_DIR)
+cmake_minimum_required(VERSION 2.8 FATAL_ERROR)
 
-FIND_PATH(MYSQL_INCLUDE_DIR mysql.h
+
+# Find mysql include dir
+find_path(MYSQL_INCLUDE_DIR mysql.h
     /usr/local/include/mysql
     /usr/include/mysql
-)
+	MYSQL/
+	C:/MinGW/include/MYSQL
+)	
 
-SET(MYSQL_NAMES mysqlclient mysqlclient_r)
-FIND_LIBRARY(MYSQL_LIBRARY
-    NAMES ${MYSQL_NAMES}
-    PATHS /usr/lib /usr/local/lib
+
+# Find mysql libs
+set(MYSQL_NAMES mysqlclient mysqlclient_r)
+find_library(MYSQL_LIBRARY NAMES ${MYSQL_NAMES}
+    PATHS /usr/lib /usr/local/lib lib/ C:/MinGW/lib 
     PATH_SUFFIXES mysql
 )
 
-IF (MYSQL_INCLUDE_DIR AND MYSQL_LIBRARY)
-    SET(MYSQL_FOUND TRUE)
-    SET( MYSQL_LIBRARIES ${MYSQL_LIBRARY} )
-ELSE (MYSQL_INCLUDE_DIR AND MYSQL_LIBRARY)
-    SET(MYSQL_FOUND FALSE)
-    SET( MYSQL_LIBRARIES )
-ENDIF (MYSQL_INCLUDE_DIR AND MYSQL_LIBRARY)
 
-IF (MYSQL_FOUND)
-    IF (NOT MYSQL_FIND_QUIETLY)
-        MESSAGE(STATUS "Found MySQL: ${MYSQL_LIBRARY}")
-    ENDIF (NOT MYSQL_FIND_QUIETLY)
-ELSE (MYSQL_FOUND)
-    IF (MYSQL_FIND_REQUIRED)
-        MESSAGE(STATUS "Looked for MySQL libraries named ${MYSQL_NAMES}.")
-        MESSAGE(FATAL_ERROR "Could NOT find MySQL library")
-    ENDIF (MYSQL_FIND_REQUIRED)
-ENDIF (MYSQL_FOUND)
+# Check final status
+if(MYSQL_INCLUDE_DIR AND MYSQL_LIBRARY)
+    set(MYSQL_FOUND TRUE)
+    set(MYSQL_LIBRARIES ${MYSQL_LIBRARY})
+
+    message(STATUS "Found MySQL: ${MYSQL_LIBRARY}")
+   
+else()
+    if(MYSQL_FIND_REQUIRED)
+	    set(MYSQL_FOUND FALSE)
+		set(MYSQL_LIBRARIES )
+	
+        message(STATUS "Looked for MySQL libraries named ${MYSQL_NAMES}.")
+        message(FATAL_ERROR "Could NOT find MySQL library")
+    endif()
+endif()
+
 
 MARK_AS_ADVANCED(MYSQL_LIBRARY MYSQL_INCLUDE_DIR)
-
